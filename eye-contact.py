7bf15155bb7a1973c691122f80cc77b5d2be3cee
trevery <canjia.mail@gmail.com>
2018-03-13 13:48:53 +0800

    display and cupture parallel is done

--- eye-contact.py
+++ eye-contact.py
@@ -1,21 +1,39 @@
-from subprocess import call
+from subprocess import call, check_output
 from pygame import key
+from multiprocessing import Process
 
-
-def cupture_video():
+def start_cupture_video():
     '''
     use shell command 'raspivid' 
     '''
-    call(['raspivid', '-o', 'eye.h264'])
+    call(['raspivid', '-t', '6000', '-n','-o', 'eye.h264']) # run background
 
+def get_cupture_PID():
+    '''
+    find PID of raspivid using shell command 'pgrep raspivid'
+    '''
+    pid = check_output(["pgrep", "raspivid"])
+    pid = pid[:-1] # drop the last char '\n'
+    return pid
+
+def stop_cupture_video():
+    '''
+    kill the PID using 'kill PID_NUM'
+    '''
+    pid = get_cupture_PID() # get_PID return a string
+    call(["kill", pid])
+
+
+    
 def convert_video():
     '''
     use shell command 'MP4Box -add video.h264 video.mp4'
     sudo apt-get install -y gpac
     '''
+    clear_eye_mp4_history()
     call(["MP4Box", "-add", "eye.h264", "eye.mp4"])
 
-def clear_history():
+def clear_eye_mp4_history():
     '''
     rm the last eye*
     '''
@@ -26,22 +44,29 @@ def display_video():
     '''
     use shell command 'omxplayer'
     '''
-    call(['omxplayer', 'eye.mp4'])
-
+    call(['omxplayer', '--loop', 'eye.mp4'])
 
 def trigger():
     '''
     read a trigger stat
     return 0 or 1 to close or open this system
     '''
-    pass
+    button = 0
+    if button_press:
+        button +=1
+        #
+        if button == 3:
+            button = 0
 
 def main():
-    cupture_video()
-    clear_history() # insure the eye.mp4 is not exist.
-    convert_video()
-    display_video()
-
+    p_display = Process(target=display_video)
+    p_cupture = Process(target=start_cupture_video)
+    #display_video()
+    #start_cupture_video()
+    p_display.start()
+    p_cupture.start()
+        
+       
 
 # option
 def eye_trigger():
@@ -49,3 +74,7 @@ def eye_trigger():
     using simplecv to detact eye blink
     return 0 or 1 to close or open this system
     '''
+
+
+if __name__ == "__main__":
+    main()
